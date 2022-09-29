071945aa4bf26ad8706a635519d531dd2f826b05
Byte Cats <devin@bytecats.codes>
2022-09-29 00:31:06 -0400

    clean up something idk

--- applogic/app.go
+++ applogic/app.go
@@ -1,31 +1,17 @@
 package applogic
 
-import (
-	"log"
-	"net/http"
-)
-
 // Api is the container for all app properties
 type Api struct {
-	Settings Settings
-	Served   Served
+    Settings Settings
+    Served   Served
 }
 
 // DefaultAPIClient is the default client for the API without config
 func DefaultAPIClient() *Api {
-	api := new(Api)
-	CheckSettings(api)
-	InitDefaultRouter(api)
-	ServerSetup(api)
+    api := new(Api)
+    CheckSettings(api)
+    InitDefaultRouter(api)
+    ServerSetup(api)
 
-	return api
-}
-
-// RunDefaultClient uses required info from Api and is starting a server
-func RunDefaultClient(api *Api) {
-	log.Printf("Server is starting on port %v", api.Settings.Port)
-	err := http.ListenAndServe(GetRnP(api))
-	if err != nil {
-		log.Printf("Can't start a server\n\"%v\"", err)
-	}
+    return api
 }
--- cmd/microguy/main.go
+++ cmd/microguy/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 
-	"microbro/applogic"
+	"microman/applogic"
 )
 
+// main is the entry point for the microguy application.
 func main() {
-	// Creating a new api instance
-	api := applogic.DefaultAPIClient()
-	// Starting the server
-	log.Println("Initializing " + applogic.GetTitle(api) + "...")
-	applogic.RunDefaultClient(api)
+    // Creating a new api instance
+    api := applogic.DefaultAPIClient()
+
+    // Starting the server
+    log.Println("Initializing " + applogic.GetTitle(api) + "...")
+    applogic.RunDefaultClient(api)
+
 }
