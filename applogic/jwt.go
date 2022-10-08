6d080f985dfa010514a54c850ea5eaada45022d6
Byte Cats <devin@bytecats.codes>
2022-10-08 13:03:32 -0400

    jwt

---
+++ applogic/jwt.go
@@ -0,0 +1,29 @@
+package applogic
+
+import (
+	"net/http"
+
+	jwtMiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
+	_ "github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func InitMiddleware(secret string) *jwtMiddleware.JWTMiddleware {
+	middleware := jwtMiddleware.New(jwtMiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		},
+		SignMethod: jwt.SigningMethodHS256,
+		Extractor:  jwtMiddleware.FromFirst(jwtMiddleware.FromAuthHeader),
+	})
+	return middleware
+}
+
+func SecureEndpoint(middleware *jwtMiddleware.JWTMiddleware, handler http.HandlerFunc, router *mux.Router) {
+	router.Handle(path, negroni.New(
+		negroni.HandlerFunc(middleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(handler)),
+	))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module microman
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/urfave/negroni v1.0.0
+)
