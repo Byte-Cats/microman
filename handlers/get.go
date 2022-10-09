5cfd58cd3e2f9b56359be8d33e487404cdac5ee8
Byte Cats <devin@bytecats.codes>
2022-10-08 23:52:52 -0400

    Comments added for Get() handler functions `/handlers/get.go`

--- handlers/get.go
+++ handlers/get.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 )
 
+// Get handler that gets something from the database according to the request received from the client in json
 func Get(w http.ResponseWriter, r *http.Request) {
 
 	_, err := w.Write([]byte("Getting something from database"))
