472d9adaec10137d79f09ae0b24607e9a5656774
Byte Cats <devin@bytecats.codes>
2022-10-08 23:57:10 -0400

    adding some ideas to login.go

--- auth/login.go
+++ auth/login.go
@@ -1,1 +1,10 @@
 package auth
+
+// This file will be used for defining login
+// and the Login function
+
+// Login will take in a username and password
+// and return a JWT token if the user is valid
+func UserLogin(username string, password string) (string, error) {
+	return "", nil
+}
