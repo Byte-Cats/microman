70e9e725c97f44fd56bb7edca08a099f55496418
Byte Cats <devin@bytecats.codes>
2022-10-30 15:50:25 -0400

    added init function to `cmd/microguy`

--- cmd/microguy/main.go
+++ cmd/microguy/main.go
@@ -7,12 +7,17 @@ import (
 	"github.com/byte-cats/microman/server"
 )
 
-// main is the entry point for the microguy application.
-func main() {
+// Init Example startup usage of microman Api
+func Init() {
 	// Creating a new api instance
 	api := al.DefaultAPIClient()
 
 	// Starting the server
 	log.Println("Initializing " + al.GetTitle(api) + "...")
 	server.RunDefaultClient(api)
+}
+
+// main is the entry point for the micro-guy application.
+func main() {
+	Init()
 }
