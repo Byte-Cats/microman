953338f95640925aacd02bd24e1842a8e94acbfc
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 18:52:47 -0500

    Update full-auth.go

--- auth/full-auth.go
+++ auth/full-auth.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
+
 const (
 	// ExpirationTime is the expiration time for the JWT token in hours.
 	ExpirationTime = 72
@@ -23,6 +24,15 @@ type User struct {
 type Claims struct {
 	ID int `json:"id"`
 	jwt.StandardClaims
+}
+
+// UserCredentialRules represents the rules for validating a user's credentials.
+type UserCredentialRules struct {
+	MinUsernameLength int
+	MaxUsernameLength int
+	MinPasswordLength int
+	AllowedUsernameSymbols string
+	DisallowedUsernameStartSymbols string
 }
 
 // generateJWT generates a JWT token for the given user ID.
@@ -45,50 +55,44 @@ func hashAndSaltPassword(password string) ([]byte, error) {
 // handleUserLogin processes a login request for the given username and password.
 // If the user is valid, it returns a JWT token. Otherwise, it returns an error.
 func handleUserLogin(username string, password string) (string, error) {
-// Validate input
-if err := validateInput(username, password); err != nil {
-return "", err
-}
-// Connect to database and retrieve user record
-user, err := getUserFromDB(username, password)
-if err != nil {
-	return "", err
+	if err := validateUserInput(username, password); err != nil {
+		return "", err
+	}
+
+	user, err := getUserFromDB(username, password)
+	if err != nil {
+		return "", err
+	}
+
+	return generateJWT(user.ID)
 }
 
-// Generate JWT token
-return generateJWT(user.ID)
-}
 
 
 // handleCreateUser processes a request to create a new user with the given username and password.
 // If the username is already in use, or if there is a problem connecting to the database, it returns an error.
 func handleCreateUser(username string, password string) error {
-	// Validate input
-	if err := validateInput(username, password); err != nil {
+	if err := validateUserInput(username, password); err != nil {
 		return err
 	}
 
-	// Check if username is already in use
 	existingUser, err := getUserFromDBByUsername(username)
 	if err == nil {
 		return errors.New("Username is already in use")
 	}
 
-	// Hash and salt password
 	hashedPassword, err := hashAndSaltPassword(password)
 	if err != nil {
 		return err
 	}
 
-	// Create new user record
-	user := User{
-		Username: username,
-		Password: string(hashedPassword),
-	}
-	if err := db.Create(&user).Error; err != nil {
-		return err
-	}
-
+	user :=User{
+Username: username,
+Password: string(hashedPassword),
+}
+if err := db.Create(&user).Error; err != nil {
+return err
+}
 	return nil
 }
 
@@ -136,30 +140,113 @@ func getUserFromDBByUsername(username string) (User, error) {
 	return user, nil
 }
 
-// validateInput checks the given username and password for invalid characters or empty values.
-// Returns an error if the input is invalid.
-func validateInput(username string, password string) error {
-	if username == "" || password == "" {
-		return errors.New("Username and password are required")
-	}
-	if containsInvalidCharacters(username) || containsInvalidCharacters(password) {
-		return errors.New("Username and password can only contain alphanumeric characters")
+// Validate checks the user's credentials against the specified rules.
+// Returns an error if the user's credentials are invalid, and nil otherwise.
+func (u *User) Validate(rules *UserCredentialRules) error {
+	if err := rules.ValidateUserCredentials(u.Username, u.Password); err != nil {
+		return err
 	}
 	return nil
+}
+
+// ValidateUserCredentials checks the given username and password against the specified rules.
+// Returns an error if either the username or password is invalid, and nil otherwise.
+func (r *UserCredentialRules) ValidateUserCredentials(username string, password string) error {
+	if len(username) < r.MinUsernameLength || len(username) > r.MaxUsernameLength {
+		return fmt.Errorf("Username must be between %d and %d characters long", r.MinUsernameLength, r.MaxUsernameLength)
+	}
+	if len(password) < r.MinPasswordLength {
+		return fmt.Errorf("Password must be at least %d characters long", r.MinPasswordLength)
+	}
+	if !r.IsUsernameValid(username) {
+		return errors.New("Username must contain only letters, numbers, and thefollowing symbols: " + r.AllowedUsernameSymbols)
+}
+return nil
+}
+
+
+// User represents a user in the system.
+type User struct {
+ID int json:"id"
+Username string json:"username"
+Password string json:"password"
+Rules *UserCredentialRules
+}
+
+// Validate checks the username and password of the user against the specified rules.
+// Returns an error if either the username or password is invalid, and nil otherwise.
+func (u *User) Validate() error {
+return u.Rules.ValidateUserCredentials(u.Username, u.Password)
+}
+
+// IsUsernameValid checks if the given username is valid according to the rules.
+// Returns true if the username is valid, and false otherwise.
+func (r *UserCredentialRules) IsUsernameValid(username string) bool {
+	if len(r.DisallowedUsernameStartSymbols) > 0 && strings.ContainsRune(r.DisallowedUsernameStartSymbols, rune(username[0])) {
+		return false
+	}
+	for _, r := range username {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) && !strings.ContainsRune(r.AllowedUsernameSymbols, r) {
+			return false
+		}
+	}
+	return true
 }
 
 // containsInvalidCharacters checks the given string for invalid characters.
 // Returns true if the string contains invalid characters, and false otherwise.
 func containsInvalidCharacters(s string) bool {
 	for _, r := range s {
-		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) && !unicode.IsSymbol(r) {
 			return true
 		}
 	}
 	return false
 }
 
+// isAlphanumeric checks if the given string consists only of alphanumeric characters.
+// It returns true if the string is alphanumeric, or false otherwise.
+func isAlphanumeric(s string) bool {
+	for _, c := range s {
+		if !(c >= '0' && c <= '9') && !(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z') {
+			return false
+		}
+	}
+	return true
+}
 
+// containsUppercase checks if the given string contains at least one uppercase letter.
+// It returns true if the string contains an uppercase letter, or false otherwise.
+func containsUppercase(s string) bool {
+	for _, c := range s {
+		if c >= 'A' && c <= 'Z' {
+			return true
+		}
+	}
+	return false
+}
+
+// containsLowercase checks if the given string contains at least one lowercase letter.
+// It returns true if the string contains a lowercase letter, or false otherwise.
+func containsLowercase(s string) bool {
+	for _, c := range s {
+		if c >= 'a' && c <= 'z' {
+			return true
+		}
+	}
+	return false
+}
+
+// containsDigit checks if the given string contains at least one digit.
+// It returns true if the string contains a digit, or false otherwise.
+func containsDigit(s string) bool {
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			return true
+		}
+	}
+	return false
+}
 
 // Encrypt will encrypt a raw string to
 // an encrypted value
@@ -192,14 +279,137 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
-	token, err := auth.UserLogin(username, password)
+	// Validate input
+	rules := UserCredentialRules{
+		MinUsernameLength: 3,
+		MaxUsernameLength: 20,
+		MinPasswordLength: 8,
+		AllowedUsernameSymbols: "_",
+		DisallowedUsernameStartSymbols: "_",
+	}
+	if err := validateInput(username, password, &rules); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Get user from database
+	user, err := getUserFromDBByUsername(username)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Compare password hash
+	if err := comparePasswordHash(user.Password, password); err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Generate JWT token
+	token, err := generateJWT(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Write([]byte(token))
 }
+
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// Invalidate the session by deleting the corresponding cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	})
+}
+
+func resetPasswordHandler(w http.ResponseWriter, r *http.Request) {
+	// Validate the request
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse the request body
+	var request struct {
+		Email string `json:"email"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the email is registered
+	user, err := getUserFromDBByEmail(request.Email)
+	if err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	// Generate a password reset token and send it to the user's email
+	token := generatePasswordResetToken(user.ID)
+	if err := sendPasswordResetEmail(user.Email, token); err != nil {
+		http.Error(w, "Failed to send password reset email", http.StatusInternalServerError)
+		return
+	}
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	// Validate the request
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse the request body
+	var request struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the user's credentials
+	rules := UserCredentialRules{
+		MinUsernameLength: 3,
+		MaxUsernameLength: 20,
+		MinPasswordLength: 8,
+		AllowedUsernameSymbols: "_",
+		DisallowedUsernameStartSymbols: "_",
+	}
+	if err := rules.ValidateUserCredentials(request.Username, request.Password); err !=
+if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	http.Error(w, "Invalid request body", http.StatusBadRequest)
+	return
+}
+
+}
+
+// Validate the user's credentials
+rules := UserCredentialRules{
+	MinUsernameLength: 3,
+	MaxUsernameLength: 20,
+	MinPasswordLength: 8,
+	AllowedUsernameSymbols: "_",
+	DisallowedUsernameStartSymbols: "_",
+}
+if err := rules.ValidateUserCredentials(request.Username, request.Password); err != nil {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+	return
+}
+
+// Create the user in the database
+if err := handleCreateUser(request.Username, request.Password); err != nil {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+	return
+}
+	
+	
 
 // Decrypt will return the original value of the encrypted string
 func Decrypt(encryptedKey []byte) ([]byte, error) {
