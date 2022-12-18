b6587b8296246f8a39485c336fdecf75747344a2
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 13:36:38 -0500

    Update jwt.go

--- auth/jwt.go
+++ auth/jwt.go
@@ -11,6 +11,8 @@ import (
 	"github.com/urfave/negroni"
 )
 
+// InitMiddleware initializes a jwtMiddleware.JWTMiddleware instance with the given secret key.
+// This middleware can be used to secure an HTTP endpoint by passing it to the SecureEndpoint function.
 func InitMiddleware(secret []byte) *jwtMiddleware.JWTMiddleware {
 	middleware := jwtMiddleware.New(jwtMiddleware.Options{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
@@ -22,6 +24,10 @@ func InitMiddleware(secret []byte) *jwtMiddleware.JWTMiddleware {
 	return middleware
 }
 
+// SecureEndpoint secures an HTTP endpoint with the given middleware.
+// When a request is made to this endpoint, the middleware will check for a valid JWT in the request header
+// and call the handler function if the JWT is valid.
+// If the JWT is invalid or not present, the middleware will return an error to the client.
 func SecureEndpoint(path string, middleware *jwtMiddleware.JWTMiddleware, handler http.HandlerFunc, router *mux.Router) {
 	router.Handle(path, negroni.New(
 		negroni.HandlerFunc(middleware.HandlerWithNext),
@@ -29,7 +35,8 @@ func SecureEndpoint(path string, middleware *jwtMiddleware.JWTMiddleware, handle
 	))
 }
 
-// FindSecret check if the secret key is set and retrieve from env
+// FindSecret retrieves the secret key from the SECRET environment variable.
+// If the SECRET environment variable is not set, the function will panic.
 func FindSecret() []byte {
 	secret := os.Getenv("SECRET")
 	if secret == "" {
