c84864e34aebd6441035b585c4701d2c0587e5e4
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:32:56 -0500

    Create constants.go

---
+++ auth/constants.go
@@ -0,0 +1,19 @@
+const (
+// Constants for string values
+	invalidUsernameLength = "Invalid username length"
+	invalidUsernameCharacters = "Invalid username characters"
+	invalidUsernameStartCharacter = "Invalid username start character"
+	invalidPasswordLength = "Invalid password length"
+	passwordMissingUppercase = "Password missing uppercase letter"
+	passwordMissingLowercase = "Password missing lowercase letter"
+	passwordMissingDigit = "Password missing digit"
+	databaseError = "Database error"
+	queryError = "Query error"
+	queryNoRows = "Query returned no rows"
+	usernameTaken = "Username already taken"
+	incorrectPassword = "Incorrect password"
+	unauthorized = "Unauthorized"
+	invalidToken = "Invalid token"
+	secretNotSetErrorMessage        = "secret value is not set"
+	negativeExpirationErrorMessage = "expiration time is negative"
+)
