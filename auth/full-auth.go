e7ea50439cedbd1ca5fbfc6785be528d72d7e3dc
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:38:18 -0500

    Update full-auth.go

--- auth/full-auth.go
+++ auth/full-auth.go
@@ -38,10 +38,7 @@ type UserCredentialRules struct {
 	
 type CredentialRules UserCredentialRules
 	
-type Secret struct {
-    Value string
-    ExpiresAt time.Time
-}
+
 	
 	
 // User represents a user in the system.
@@ -87,30 +84,6 @@ func CloseDB(db *sql.DB) error {
 
 
 
-func FindSecret(secretEnvVar, expirationEnvVar, defaultSecret, defaultExpiration string) (*Secret, error) {
-	secretValue := getenv(secretEnvVar, defaultSecret)
-	if secretValue == "" {
-		return nil, errors.New(secretNotSetErrorMessage)
-	}
-
-	expiration, err := time.ParseDuration(getenv(expirationEnvVar, defaultExpiration))
-	if err != nil {
-		return nil, err
-	}
-	if expiration < 0 {
-		return nil, errors.New(negativeExpirationErrorMessage)
-	}
-
-	secretBits, err := hex.DecodeString(secretValue)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Secret{
-		Value: secretBits,
-		ExpiresAt: time.Now().Add(expiration),
-	}, nil
-}
 
 
 
