cc367ca3fe36ab2b5ac161a8de12631c43232fd8
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:31:57 -0500

    Update full-auth.go

--- auth/full-auth.go
+++ auth/full-auth.go
@@ -151,11 +151,7 @@ func FindSecret(secretEnvVar, expirationEnvVar, defaultSecret, defaultExpiration
 	}, nil
 }
 
-// hashAndSaltPassword hashes and salts the given password using bcrypt.
-// Returns the hashed password as a byte slice, and any error that occurred.
-func hashAndSaltPassword(password string) ([]byte, error) {
-	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-}
+
 
 // containsUppercase checks if the given string contains at least one uppercase letter.
 // It returns true if the string contains an uppercase letter, or false otherwise.
@@ -421,36 +417,6 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func resetPasswordHandler(w http.ResponseWriter, r *http.Request) {
-	// Validate the request
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Parse the request body
-	var request struct {
-		Email string `json:"email"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-
-	// Check if the email is registered
-	user, err := getUserFromDBByEmail(request.Email)
-	if err != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	// Generate a password reset token and send it to the user's email
-	token := generatePasswordResetToken(user.ID)
-	if err := sendPasswordResetEmail(user.Email, token); err != nil {
-		http.Error(w, "Failed to send password reset email", http.StatusInternalServerError)
-		return
-	}
-}
 
 
 
@@ -482,44 +448,6 @@ func Decrypt(encryptedKey []byte) ([]byte, error) {
 	)
 }
 
-// ChangePassword takes in a user ID, the current password, and the new password and updates the user's password in the database if the current password is correct. Returns an error if the current password is incorrect or there is an error updating the password.
-func ChangePassword(userID int, currentPassword string, newPassword string) error {
-	// connect to database and retrieve user record
-	user, err := getUserFromDB(userID)
-	if err != nil {
-		return err
-	}
-
-	// check if current password is correct
-	if user.Password != currentPassword {
-		return errors.New("Incorrect current password")
-	}
-
-	// update password in database
-	user.Password = newPassword
-	if err := updateUserInDB(user); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// ResetPassword takes in a user ID and a new password and resets the user's password in the database. Returns an error if there is an error resetting the password.
-func ResetPassword(userID int, newPassword string) error {
-	// connect to database and retrieve user record
-	user, err := getUserFromDB(userID)
-	if err != nil {
-		return err
-	}
-
-	// update password in database
-	user.Password = newPassword
-	if err := updateUserInDB(user); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 // Logout takes in a JWT token and invalidates it so that it can no longer be used for authentication.
 func Logout(token string) error {
