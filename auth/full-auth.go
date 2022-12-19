1a3bb571c6bef25f5d1dee5d8f853dd0f4175ed1
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:33:25 -0500

    Update full-auth.go
    constants to another file

--- auth/full-auth.go
+++ auth/full-auth.go
@@ -32,24 +32,6 @@ const (
 	// ExpirationTime is the expiration time for the JWT token in hours.
 	ExpirationTime = 72
 	jwtSigningMethod = jwt.SigningMethodHS256
-	
-	// Constants for string values
-	invalidUsernameLength = "Invalid username length"
-	invalidUsernameCharacters = "Invalid username characters"
-	invalidUsernameStartCharacter = "Invalid username start character"
-	invalidPasswordLength = "Invalid password length"
-	passwordMissingUppercase = "Password missing uppercase letter"
-	passwordMissingLowercase = "Password missing lowercase letter"
-	passwordMissingDigit = "Password missing digit"
-	databaseError = "Database error"
-	queryError = "Query error"
-	queryNoRows = "Query returned no rows"
-	usernameTaken = "Username already taken"
-	incorrectPassword = "Incorrect password"
-	unauthorized = "Unauthorized"
-	invalidToken = "Invalid token"
-	secretNotSetErrorMessage        = "secret value is not set"
-	negativeExpirationErrorMessage = "expiration time is negative"
 )
 	
 // UserCredentialRules represents the rules for validating a user's credentials.
