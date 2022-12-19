1c691497f354ae4ef25391c6c959aa72046ba8be
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:46:53 -0500

    Create login.go

---
+++ auth/login.go
@@ -0,0 +1,79 @@
+
+
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	// Validate input
+	rules := UserCredentialRules{
+		MinUsernameLength: 3,
+		MaxUsernameLength: 20,
+		MinPasswordLength: 8,
+		AllowedUsernameSymbols: "_",
+		DisallowedUsernameStartSymbols: "_",
+	}
+	if err := validateInput(username, password, &rules); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Get user from database
+	user, err := getUserFromDBByUsername(username)
+	if err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Compare password hash
+	if err := comparePasswordHash(user.Password, password); err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Generate JWT token
+	token, err := generateJWT(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(token))
+}
+
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// Invalidate the session by deleting the corresponding cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	})
+}
+
+
+
+// Logout takes in a JWT token and invalidates it so that it can no longer be used for authentication.
+func Logout(token string) error {
+	// parse token to get claims
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return FindSecret(), nil
+	})
+	if err != nil {
+		return err
+	}
+	claims := parsedToken.Claims.(jwt.MapClaims)
+
+	// get user ID from claims
+	userID, ok := claims["id"].(float64)
+	if !ok {
+		return errors.New("Invalid token")
+	}
+
+	// invalidate token in database
+	if err := invalidateTokenInDB(int(userID)); err != nil {
+		return err
+	}
+
+	return nil
+}
