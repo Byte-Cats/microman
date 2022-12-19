ddc1e468f259ea168907352087525d83133307c5
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:45:19 -0500

    Create database.go

---
+++ auth/database.go
@@ -0,0 +1,34 @@
+
+var database *sql.DB
+
+func initDB() {
+	var err error
+	db, err = ConnectDB("user", "password", "localhost", "3306", "auth")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+	
+
+func ConnectDB(user, password, host, port, dbname string) (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbname)
+	database, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = database.Ping(); err != nil {
+		return nil, err
+	}
+
+	return database, nil
+}
+
+
+func CloseDB(db *sql.DB) error {
+	return db.Close()
+}
+	
+
+
+
