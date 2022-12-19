7f6b23aedba094aa5262727f10ba4be2b1923958
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-18 21:38:20 -0500

    Create secrets.go

---
+++ auth/secrets.go
@@ -0,0 +1,33 @@
+
+
+type Secret struct {
+    Value string
+    ExpiresAt time.Time
+}
+
+
+
+func FindSecret(secretEnvVar, expirationEnvVar, defaultSecret, defaultExpiration string) (*Secret, error) {
+	secretValue := getenv(secretEnvVar, defaultSecret)
+	if secretValue == "" {
+		return nil, errors.New(secretNotSetErrorMessage)
+	}
+
+	expiration, err := time.ParseDuration(getenv(expirationEnvVar, defaultExpiration))
+	if err != nil {
+		return nil, err
+	}
+	if expiration < 0 {
+		return nil, errors.New(negativeExpirationErrorMessage)
+	}
+
+	secretBits, err := hex.DecodeString(secretValue)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Secret{
+		Value: secretBits,
+		ExpiresAt: time.Now().Add(expiration),
+	}, nil
+}
